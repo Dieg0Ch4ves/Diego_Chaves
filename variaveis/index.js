10567cda4b544696d5a2dc628a07866f30e01fd4
Diego Chaves <diegoaraujo200570@gmail.com>
2022-04-23 12:08:27 -0400

    Atividades De Variaveis

--- variaveis/index.js
+++ variaveis/index.js
@@ -1,5 +1,34 @@
 console.log("OLA CADELA")
 
-const nome = prompt("Qual é o seu nome ?")
-let idade = Number (prompt("Qual é a sua idade ?"))
-///CHinelo
+//Exercicio 1
+//const nome = prompt("Qual é o seu nome ?")
+//let idade = Number (prompt("Qual é a sua idade ?"))
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+//porque na linha 3 não foi dado o valor,sendo assim, a linha 3 é "string".
+//E na linha 4 foi dado o valor de "Number", sendo assim, o tipo é de Number.
+
+//console.log("Olá,",nome, ", Você tem",idade,"Anos")
+
+//Exercicio 2
+//const aula = prompt("Você foi na aula ontem ?")
+//const conteudo = prompt("Copiou o conteudo que o professor pasoou ?")
+//const passar = prompt("poderia me passar depois ?")
+
+//console.log("Você foi na aula ontem ?",aula)
+//console.log("Copiou o conteudo que o professor pasoou ?",conteudo)
+//console.log("poderia me passar depois ?",passar)
+
+//Exercicio 3
+
+//let a = Number(10)
+//let b = Number(20)
+
+//console.log("o valor de a é" ,a)
+//console.log("o valor de b é", b)
+a = a + 10
+b = b - 10
+//console.log("o novo valor de a agora é", a)
+//console.log("o novo valor de b agora é", b)
