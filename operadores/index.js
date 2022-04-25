4c7684e3e5b8490436ee408847021236d96797da
Diego Chaves <diegoaraujo200570@gmail.com>
2022-04-25 10:59:55 -0400

    Atividades operadores

---
+++ operadores/index.js
@@ -0,0 +1,115 @@
+//Exercicios de Operadores aritméticos
+//Exercicio 1
+
+//const tres = 3
+//const quatro = 4
+
+//const resultado = tres + quatro
+
+//console.log (resultado)
+
+//Exercicio 2
+
+//const tres = 3 
+//const cinco = 5
+//const resultado = tres * cinco 
+
+//console.log (resultado / 2)
+
+//Exercicio 3
+
+//const cinco = 5
+//const quatro = 4
+//const resultado = cinco - quatro
+
+//console.log (resultado * -1)
+
+//Exercicio 4
+
+//const a = 234
+//const b = 5
+//const resultado = a / b
+
+//console.log (resultado)
+
+//Exercicio de Comparadores 
+
+//const a = 5
+//const b = 10
+
+//console.log (a === b)
+//console.log (a !== b)
+//console.log (a > b)
+//console.log (a < b)
+
+//Exercicio de Operadores Logicos
+//Exercicio 1
+
+//const a = true
+//const b = false
+//const c = true
+
+//console.log (a && b)
+//console.log (b && c)
+//console.log (a && c)
+//console.log (a && b && c)
+
+//Exercicio 2
+
+//const a = true
+//const b = false
+//const c = true
+
+//console.log (a || b)
+//console.log (b || c)
+//console.log (a || c)
+//console.log (a || b || c)
+
+//Exercicio 5
+
+//const nome = "Lais"
+//const idade = 23
+//const maiordeidade = true
+//const anode2050 = idade + 29
+
+//console.log ("Ola,", nome)
+//console.log ("Você tem", idade, "anos")
+//console.log ("Você é maior de idade ?", maiordeidade)
+//console.log ("Em 2050 você terá", anode2050)
+
+//Exercicio 1 discord
+
+//const idade = Number (prompt("Qual é a sua idade ?"))
+//console.log ("Sua idade,", idade, "anos")
+
+//const idadedoamigo = Number (prompt("Qual a idade do seu melhor amigo(a) ?"))
+//console.log ("Idade do seu melhor amigo(a),", idadedoamigo, "anos")
+
+//console.log ("Sua idade é maior do que a do seu amigo(a) ?", idade > idadedoamigo)
+
+//console.log ("A diferença de idade entro vocês é ?", idade - idadedoamigo, "anos")
+
+//Exercicio 2 discord
+
+//const numeropar = Number(prompt("Insira um número par"))
+//console.log (numeropar % 2)
+
+//numeros pares sempre darão 0, enquanto numeros impares sempre sobrara 1.
+
+//Exercicio 3 discord
+
+//const idade = Number(prompt("Qual a sua idade em anos ?"))
+//console.log ("Sua idade em anos,", idade, "anos")
+//console.log ("Sua idade em meses,", idade * 12, "meses")
+//console.log ("Sua idade em dias,", idade * 365, "dias")
+//console.log ("Sua idade em horas,", idade * 8760, "horas")
+
+//Exercicio 4  discord
+
+const a = Number(prompt("Insira um numero"))
+const b = Number(prompt("Insira outro"))
+
+console.log ("O primeiro número é maior que o segundo ?", a > b)
+console.log ("O primeiro numero é igual ao segundo ?", a === b)
+console.log ("O primeiro numero é divisível pelo segundo ?", a % b === 0)
+console.log ("O segundo numero é divisível pelo primeiro ?", b % a === 0)
