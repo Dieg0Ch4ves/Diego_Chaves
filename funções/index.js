565be3c2f4e423e408309401cb3fd0300009b2a7
Diego Chaves <diegoaraujo200570@gmail.com>
2022-04-28 14:49:20 -0400

    Atividades de Funções

---
+++ funções/index.js
@@ -0,0 +1,92 @@
+//Exercicios De Funções
+//Exercicio 1
+
+//function oi(nome) {
+//    console.log ("Olá,",nome)
+//
+//    return nome
+//}
+//
+//oi("Diego")
+//oi("Thiago")
+//oi("Geovana")
+
+//Exercicio 2
+
+//const a = 20
+//const b = 10
+//function soma(){
+//    console.log (a + b)
+//}
+//soma()
+
+//Exercicio 3
+
+//const comprimento = function(nome){
+//    console.log ("Olá,", nome)
+//}
+//
+//comprimento("Diego")
+//
+//
+//const somar = (a, b) => {
+//    return a + b
+//}
+//
+//console.log (somar(20, 10))
+
+//Exercicio 1 discord
+//const nome = prompt("SEU NOME")
+//const idade = prompt("Sua idade")
+//const endereço = prompt("Seu endereço")
+//const profissao = prompt("Sua profissão")
+//const mensagem = (nome, idade, endereço, profissao) => {
+//    
+//    
+//    console.log (`Eu sou ${nome}, tenho ${idade} anos, moro em ${endereço} e sou ${profissao}.`)
+//    return mensagem
+//}
+//mensagem(nome, idade, endereço, profissao)
+
+//Exercicio 2 discord
+
+//const somar = (a,b) => {
+//    return a + b
+//}
+//console.log (somar(20, 10))
+
+//const somar = (a,b) => {
+//    return a > b
+//}
+//console.log (somar(20, 10))
+
+//const somar = (a) => {
+//    return a % 2 === 0
+//}
+//console.log (somar(21))
+
+//const formatar = (mensagem) => {
+//    return `${mensagem.length}, ${mensagem.toUpperCase()}`
+//}
+//console.log (formatar("BALDe"))
+
+//Exercicio 3 discord
+
+//const a = Number (prompt("Fale um numero"))
+//const b = Number (prompt("Diga outro"))
+//const somar = (a,b) => {
+//    return a + b
+//}
+//const tirar = (a,b) => {
+//    return a - b
+//}
+//const multiplicar = (a,b) => {
+//    return a * b
+//}
+//const dividir = (a,b) => {
+//    return a /  b
+//}
+//console.log (somar(a,b))
+//console.log (tirar(a,b))
+//console.log (multiplicar(a,b))
+//console.log (dividir(a,b))
