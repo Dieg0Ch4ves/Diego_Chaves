b29e33fc0498c05bbc6adee0831abeaa9293c959
Diego Chaves <diegoaraujo200570@gmail.com>
2022-05-23 12:32:13 -0400

    Atividades de loop

--- Laços(loops)/index.js
+++ Laços(loops)/index.js
@@ -14,7 +14,7 @@
 // function maior (n) {
 //     let m = 0
 //     for(let x = 0; x < n.length; x++){
-        
+
 //         if(m < n[x]){
 //             console.log(`O número ${m} é o menor que ${n[x]}`)
 //             m = n[x]
@@ -32,7 +32,6 @@
 
 //Exercicio 1 discord
 
-
 // const pets = Number (prompt("Quantos Pets você tem ?"))
 // let i = []
 
@@ -48,17 +47,48 @@
 // console.log (pets)
 
 //Exercicio 2 discord
-
-const origin = [5,10,20]
+//
+//const banana = [5, 10, 20]
 // function dividi(origin) {
 //     console.log (origin[0] / 10,)
 //     console.log (origin[1] / 10,)
 //     console.log (origin[2] / 10)
 // }
-// dividi(origin)
-
-function par(origin) {
-        while(origin[0] === 0) {
-            origin[0] % 2
-            origin[0] = origin [0] + 1
-        }
+//dividi(banana)
+//function numpares(origin) {
+//    let a = [];
+//  for (pares of origin) {
+//    if (pares % 2 === 0) {
+//      console.log(pares);
+//      a.push(pares);
+//    }
+//  }
+//  return a
+//}
+//console.log(numpares(banana))
+//
+//function string(banana) {
+//  let b = []
+//  let a = []
+//  for(index of banana) {
+//    let b = [`O elemento do índex banana é: ${index}`]
+//    a.push(b)
+//  }
+//  return a
+//}
+//console.log(string(banana))
+//
+//function mn(banana) {
+//  let c = 11
+//  let b = 6
+//  let a = []
+//  for(index of banana) {
+//    if(index > c){
+//      a.push(index)
+//    }if(index < b){
+//      a.push(index)
+//    }
+//  }
+//  return a
+//}
+//console.log (mn(banana))
